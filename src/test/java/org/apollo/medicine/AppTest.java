21eabc4b2ee44c51d269188f66e926f602c8870b
root <deyvesshkumar@gmail.com>
2025-09-29 00:16:56 +0530

    added files

---
+++ src/test/java/org/apollo/medicine/AppTest.java
@@ -0,0 +1,14 @@
+package org.apollo.medicine;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void dummyTest() {
+        // Simple test that always passes
+        assertTrue(true, "This test should always pass");
+    }
+}
+
